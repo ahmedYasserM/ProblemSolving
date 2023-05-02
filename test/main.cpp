1aaf5d51738be1f09e07084dc5e58d40c5440ff5
Ahmed Yasser <ahmedyassermohammad@gmail.com>
2023-05-02 06:59:15 +0300

    :memo: Adding new-password problem

---
+++ test/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+#define min std::min
+#define vector std::vector
+#define cin std::cin
+#define cout std::cout
+#define endl std::endl
+#define string std::string
+
+
+int main (int argc, char *argv[])
+{
+  
+  string x = "";
+
+  x += 0 + 97;
+
+
+
+  return 0;
+}
